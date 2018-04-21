53f4aa067ed4df59ded1d3ceece9fcba06ef9a4e
Swynfel <swynfel@gmail.com>
2018-04-21 19:24:14 +0200

    Made level parser

---
+++ Assets/Scripts/Gameplay/LevelGeneratorParserer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public static class LevelGeneratorParserer {
+
+    public static void ParseFile(this LevelGenerator levelGenerator, string file)
+    {
+        string[] lines = File.ReadAllLines(file);
+        List<LevelSequence> sequences = levelGenerator.m_orderSequences = new List<LevelSequence>();
+        LevelSequence currentSequence = new LevelSequence();
+        foreach (string line in lines)
+        {
+            try
+            {
+                string[] words = line.Split(' ');
+                if(words.Length==0)
+                {
+                    continue;
+                }
+                LevelSequence.LevelSequenceStart type = MakeLevelSequence(words[0]);
+                //New sequence
+                if(type!=LevelSequence.LevelSequenceStart.ERROR)
+                {
+                    if(currentSequence.orders.Count>0 && currentSequence.start){
+                        sequences.Add(currentSequence);
+                    }
+                    currentSequence = new LevelSequence();
+                    currentSequence.start = type;
+                }
+                //New order
+                LevelOrder order = MakeLevelOrder(words);
+
+            }
+            catch
+            {
+                Debug.LogWarning("Couldn't read line \"" + line + "\" of file \"" + file + "\"");
+            }
+        }
+    }
+
+    public static T[] SubArray<T>(this T[] data, int index, int length=-1)
+    {
+        if (length == -1)
+        {
+            length = data.Length - index;
+        }
+        T[] result = new T[length];
+        Array.Copy(data, index, result, 0, length);
+        return result;
+    }
+
+    static protected LevelSequence.LevelSequenceStart MakeLevelSequence(string type)
+    {
+        return LevelSequence.LevelSequenceStart.ERROR;
+    }
+
+    static protected LevelOrder MakeLevelOrder(string[] args){
+        System.DateTime time = System.DateTime.Parse(args[0], "mm:ss.fff");
+        float floatTime = time.Millisecond;
+        switch (args[1].ToLower())
+        {
+            case("spawn"):
+                return new SpawnLevelOrder(args.SubArray(2));
+
+            case("talk"):
+                //TODO
+                break;
+
+            case("music"):
+                //TODO
+                break;
+
+            case("endgame"):
+                //TODO
+                break;
+        }
+        return null;
+    }
+        
+    static public GameObject GetPrefab(string name){
+        return (GameObject)Resources.Load("Prefabs/"+name, typeof(GameObject));
+    }
+}
---
+++ Assets/Scripts/Gameplay/LevelOrder.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct LevelSequence
+{
+
+	public enum LevelSequenceStart
+	{
+		NOW,
+		WHEN_DONE,
+        ERROR
+	}
+
+	public LevelSequenceStart start = LevelSequenceStart.ERROR;
+    public List<LevelOrder> orders = new List<LevelOrder>();
+
+}
+
+public enum LevelOrderType
+{
+	SPAWN,
+	MUSIC,
+    TALK,
+	END_LEVEL
+}
+
+public abstract class LevelOrder
+{
+	public LevelOrderType orderType
+	{
+		get;
+		protected set;
+	}
+	public float when;
+
+	public string ToString(){
+		return orderType.ToString () + ":" + when.ToString ();
+	}
+}
+
+public class SpawnLevelOrder : LevelOrder
+{
+	public LevelOrderType orderType = LevelOrderType.SPAWN;
+	public List<GameObject> entities;
+    public List<int> entitiesQuantity;
+
+    public SpawnLevelOrder(string[] args)
+    {
+        for (int k = 0; k < args.Length; k++)
+        {
+            string[] things = args[k].Split("*");
+            string prefab;
+            int num = 1;
+            if (things.Length > 1)
+            {
+                num = (int)things[0];
+                prefab = things[1];
+            }
+            else
+            {
+                prefab = things[0];
+            }
+            entities.Add(LevelGeneratorParserer.GetPrefab(prefab));
+            entitiesQuantity.Add(num);
+        }
+    }
+}
+
+public class MusicLevelOrder : LevelOrder
+{
+	public LevelOrderType orderType = LevelOrderType.MUSIC;
+	public string music; //TODO
+}
+
+public class TalkLevelOrder : LevelOrder
+{
+    public LevelOrderType orderType = LevelOrderType.MUSIC;
+    public string face; //TODO
+    public string text; //TODO
+}
+
+public class EndLevelOrder : LevelOrder
+{
+	public LevelOrderType orderType = LevelOrderType.END_LEVEL;
+}
