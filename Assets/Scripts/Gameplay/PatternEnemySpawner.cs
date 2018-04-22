0801ff872037a92ce5258f1bb1073a5b921473e3
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-04-22 20:30:05 +0200

    add squadron pattern, fix invulnerabilty frames

--- Assets/Scripts/Gameplay/PatternEnemySpawner.cs
+++ Assets/Scripts/Gameplay/PatternEnemySpawner.cs
@@ -5,11 +5,8 @@ using UnityEditor;
 
 public class PatternEnemySpawner : MonoBehaviour
 {
-	[SerializeField] private Transform m_SpawningLocation;
-	[SerializeField] private Transform m_ShootDirection;
 	[SerializeField] private List<GameObject> m_EnemyPrefabs;
 	[SerializeField] private float m_EnemySpawningDelay;
-	[SerializeField] private BezierCurve m_Curve;
 
 	private float m_Delay;
 	private int currentIndex;
@@ -19,34 +16,16 @@ public class PatternEnemySpawner : MonoBehaviour
 		m_Delay = m_EnemySpawningDelay;
 		currentIndex = 0;
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
-		if (m_Delay < m_EnemySpawningDelay)
-		{
+		if (m_Delay < m_EnemySpawningDelay) {
 			m_Delay += Time.deltaTime;
-		}
-		else
-		if (currentIndex < m_EnemyPrefabs.Count)
-		{
+		} else if (currentIndex < m_EnemyPrefabs.Count) {
 			m_Delay = 0;
 			GameObject enemy = Instantiate (m_EnemyPrefabs [currentIndex]);
-            if (m_ShootDirection)
-            {
-                EnemyAI ai = enemy.GetComponent<EnemyAI>();
-                ai.SetShootDirection(m_ShootDirection);
-            }
-            if (m_Curve)
-            {  
-                EnemyPath path = enemy.GetComponent<EnemyPath>();
-                path.SetPath(m_Curve);
-            }
-			enemy.transform.position = m_SpawningLocation.position;
 			currentIndex++;
-		}
-		else
-		{
+		} else {
 			Destroy (this);
 		}
 	}
