74ae34e089c6ae54245e7f6332fbf0a0cd0784ac
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-04-22 10:12:43 +0200

    add dialogue system

---
+++ Assets/Scripts/Engine/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Dialogue
+{
+	public string m_Name;
+	[TextArea (3, 10)]
+	public string[] m_Sentences;
+}
+
---
+++ Assets/Scripts/Engine/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueTrigger : MonoBehaviour
+{
+	public Dialogue m_Dialogue;
+
+	public void TriggerDialogue ()
+	{
+		DialogueManager.instance.StartDialogue (m_Dialogue);
+	}
+}
+
